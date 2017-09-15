7b08b9e6065ef7b635a8999b941bd543ad91ecfd
dionefraga <dionefraga@gmail.com>
2017-09-15 16:23:13 -0300

    getting health brazil-rs info from twitter by twython

--- twitter_health_BR_rs2.py
+++ twitter_health_BR_rs2.py
@@ -15,7 +15,24 @@ AccessTokenSecret = ''
 twitter = Twython(ConsumerKey, ConsumerSecret, AccessToken, AccessTokenSecret)
 
 result = twitter.search(q="((Saúde OR SUS) AND (Rio Grande do Sul OR RS) OR (SECRETARIA DE SAÚDE OR SECRETARIA DE SAÚDE DO ESTADO DO RIO GRANDE DO SUL OR SECRETARIA DE SAÚDE DO ESTADO DO RS))",
-                        locale='BR', result_type='recent', count=10)
+                        result_type='recent', count=10)
 
-for status in result['statuses']:
-    print("user: {0} text: {1}".format(status["user"]["name"], status["text"]))
+
+#print(type(result))
+#for status in result['statuses']:
+#    print("user: {0} text: {1}".format(status["user"]["name"], status["text"]))
+    
+
+# Initialize empty list to store tweets: tweets_data
+tweets_data = []   
+    
+    
+#df = pd.DataFrame.from_dict(result["data"])
+dict_normalized = pd.io.json.json_normalize(result)
+df = pd.DataFrame(dict_normalized)
+
+for i in df['statuses']:
+    print(i)
+    
+print(df.keys())
+print(type(df['statuses']))
